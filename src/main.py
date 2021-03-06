2b13a23d2fbc0d682e0aed47d547449ef0cc43af
Julián Velandia <jvelandiag@unal.edu.co>
2021-03-05 22:44:43 -0500

    first commit

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+import sys
+
+
+
+
+
+
+
+if __name__=='__main__':
+    app = QApplication([])
+    ventana = QMainWindow()
+    ventana.show()
+    app.exec_()
