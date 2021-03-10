12cd1263a1d3da9985725df1c6d58bf6d0cc4c74
Julián Camilo Velandia <jvelandiag@unal.edu.co>
2021-03-09 21:02:36 -0500

    4 video sin sql

--- src/textInput/main.py
+++ src/textInput/main.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -14,32 +16,39 @@ class MainApp(QMainWindow):
         self.setWindowTitle('Input')   #titulo
 
 
+        self.centralWidget = QWidget()
+        self.setCentralWidget(self.centralWidget)
+
+
         '''
         Campo de texto y  de contraseña
         '''
-        self.user_input = QLineEdit(self)   #caja de texto
-        self.user_input.setPlaceholderText("User")
+        self.user_input = QLineEdit(self.centralWidget)   #caja de texto
+        self.user_input.setPlaceholderText("UserName")
         self.user_input.setClearButtonEnabled(True)
         self.user_input.setGeometry(220,10,150,30)
         self.user_input.setMaxLength(15)
 
-        self.user_password = QLineEdit(self)   #caja de texto
+        self.user_password = QLineEdit(self.centralWidget)   #caja de texto
         self.user_password.setPlaceholderText("Password")
         self.user_password.setClearButtonEnabled(True)
         self.user_password.setGeometry(220,50,150,30)
         self.user_password.setMaxLength(10)
         self.user_password.setEchoMode(QLineEdit.Password)
 
-        self.login_button = QPushButton('Login',self)
+        self.login_button = QPushButton('Login',self.centralWidget)
         self.login_button.setGeometry(220, 90, 150, 30)
 
         '''
         Triggers
         '''
-        self.user_input.returnPressed.connect(self.show_text)
+        self.login_button.clicked.connect(self.login)
 
-    def show_text(self):
-        print(self.user_input.text())
+    def login(self):
+        user_name = self.user_input.text()
+        password = self.user_password.text()
+        if user_name != '' and password != '' :
+            print('correcto')
 
 
 
